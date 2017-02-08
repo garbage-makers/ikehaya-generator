78b4361bc058a9d2cdc2fac007616ce312446e04
kojimakeisuke <kojima.keisuke11@gmail.com>
2017-02-08 23:49:16 +0900

    :mushroom:
    :mushroom:

--- ikehaya.js
+++ ikehaya.js
@@ -62,7 +62,7 @@ function generate(){
   var source = '出典：　<a target="_blank" href="' + links[rand] + '">' + links[rand] + '</a>';
   $(".tweet").append(tweet);
   $(".source").append(source);
-  document.title = tweet + " / イケダハヤトジェネレーター http://bit.ly/2kSGU0H";
+  document.title = tweet + " / イケダハヤトジェネレーター http://bit.ly/2kHGg4e";
   var tw = '<a href="https://twitter.com/share" class="twitter-share-button" data-related="ihayato">Tweet</a>';
   tw += '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?';
   tw += "'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>"
