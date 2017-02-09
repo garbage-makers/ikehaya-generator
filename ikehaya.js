a4e29b189c38f1ac775d7f322eca92c4e4fa6c1a
kojimakeisuke <kojima.keisuke11@gmail.com>
2017-02-09 15:52:56 +0900

    mobile対応

--- ikehaya.js
+++ ikehaya.js
@@ -59,10 +59,10 @@ function generate(){
   var arrayLength = ikehaya.length;
   var rand = Math.floor( Math.random() * arrayLength) ;
   var tweet = material + "\n" + ikehaya[rand];
-  var source = '出典：　<a target="_blank" href="' + links[rand] + '">' + links[rand] + '</a>';
+  var source = '（出典）<br /><a target="_blank" href="' + links[rand] + '">' + links[rand] + '</a>';
   $(".tweet").append(tweet);
   $(".source").append(source);
-  document.title = tweet + " / イケダハヤトジェネレーター http://bit.ly/2kHGg4e";
+  document.title = tweet + " / イケダハヤトジェネレーター http://bit.ly/2kSGU0H";
   var tw = '<a href="https://twitter.com/share" class="twitter-share-button" data-related="ihayato">Tweet</a>';
   tw += '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?';
   tw += "'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>"
